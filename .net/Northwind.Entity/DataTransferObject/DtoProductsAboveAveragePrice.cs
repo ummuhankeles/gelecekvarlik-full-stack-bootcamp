9ecde32915117979e2d25dda669cedf469c9f33b
Ümmühan Keleş <ummuhankeles4@gmail.com>
2022-04-20 15:35:11 +0300

    Update inheritance name in dto

--- .net/Northwind.Entity/DataTransferObject/DtoProductsAboveAveragePrice.cs
+++ .net/Northwind.Entity/DataTransferObject/DtoProductsAboveAveragePrice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Northwnd.Entity.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Northwnd.Entity.DataTransferObject
 {
-    public class DtoProductsAboveAveragePrice : Dtobase
+    public class DtoProductsAboveAveragePrice : DtoBase
     {
         public string ProductName { get; set; }
         public decimal? UnitPrice { get; set; }
